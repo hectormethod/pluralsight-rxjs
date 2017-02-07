58ee14a5006a3f6ba301cce02f5556eb197a38f0
scott-pc <skesler@outlook.com>
2017-02-06 22:16:54 -0600

    initial commit

---
+++ main.ts
@@ -0,0 +1,27 @@
+import {Observable} from "rxjs";
+
+let numbers = [1,2,3,9,10];
+let source = Observable.create(observer => {
+
+    let index = 0;
+    let produceValue = () => {
+        observer.next(numbers[index++]);
+
+        if (index < numbers.length){
+            setTimeout(produceValue, 250);
+        } else {
+            observer.complete();
+        }
+    }
+
+    produceValue();
+}).map( n => n * 2)
+    .filter( n => n > 4);
+
+
+source.subscribe(
+    value => console.log(`This is the subscribe to Observable value: ${value}`),
+    e =>  console.log(`error: ${e}`),
+    () => console.log(`complete`)
+);
+
