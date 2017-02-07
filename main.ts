1984486d442fc680de3fa87fa5efbb2107cbc0d2
scott-pc <skesler@outlook.com>
2017-02-06 22:44:53 -0600

    added traveling circle

--- main.ts
+++ main.ts
@@ -1,26 +1,24 @@
 import {Observable} from "rxjs";
 
-let numbers = [1,2,3,9,10];
-let source = Observable.create(observer => {
-
-    let index = 0;
-    let produceValue = () => {
-        observer.next(numbers[index++]);
-
-        if (index < numbers.length){
-            setTimeout(produceValue, 250);
-        } else {
-            observer.complete();
-        }
+let circle = document.getElementById("circle");
+let source = Observable.fromEvent(document, "mousemove")
+    .map((e : MouseEvent) => {
+    return {
+        x: e.clientX,
+        y: e.clientY
     }
 
-    produceValue();
-}).map( n => n * 2)
-    .filter( n => n > 4);
+    })
+    .filter(value => value.x < 500);
 
+
+function onNext(value){
+    circle.style.left = value.x;
+    circle.style.top = value.y;
+}
 
 source.subscribe(
-    value => console.log(`This is the subscribe to Observable value: ${value}`),
+    onNext,
     e =>  console.log(`error: ${e}`),
     () => console.log(`complete`)
 );
