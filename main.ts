c0245cb8bbe784f944bc99347c67dba9614afb72
scott-pc <skesler@outlook.com>
2017-02-09 19:53:29 -0600

    error coding

--- main.ts
+++ main.ts
@@ -1,25 +1,69 @@
 import {Observable} from "rxjs";
 
-let circle = document.getElementById("circle");
-let source = Observable.fromEvent(document, "mousemove")
-    .map((e : MouseEvent) => {
-    return {
-        x: e.clientX,
-        y: e.clientY
-    }
+let button = document.getElementById("moviesButton");
+let output = document.getElementById("output");
 
-    })
-    .filter(value => value.x < 500);
+let click = Observable.fromEvent(button, "click");
+
+function load(url: string) {
+    return Observable.create(observer => {
+
+        let xhr = new XMLHttpRequest();
+        // load is standard event raised when data is finished
 
 
-function onNext(value){
-    circle.style.left = value.x;
-    circle.style.top = value.y;
+        xhr.addEventListener("load", () => {
+            if(xhr.status === 200) {
+                let data = JSON.parse(xhr.responseText);
+                observer.next(data);
+            } else {  //error 500
+
+                observer.error(xhr.statusText);
+            }
+
+        });
+
+        xhr.open("GET", url);
+        xhr.send();
+    }).retryWhen( retryStrategy( {attempts: 8 , delay: 700}));
+};
+//this is doing all the work of the above load() method
+function loadWithFetch(url: string){
+    return Observable.defer( () => {
+        return  Observable.fromPromise(fetch(url).then(r => r.json()));
+    });
 }
 
-source.subscribe(
-    onNext,
-    e =>  console.log(`error: ${e}`),
+function retryStrategy({attempts, delay}){
+    return function(errors){
+        return errors
+            .scan( (acc, value) => {
+                console.log(acc, value);
+                return acc + 1;
+            }, 0)
+            .takeWhile (acc => acc < attempts )
+            .delay(delay);
+    }
+}
+
+function renderMovies(movies) {
+    movies.forEach(m => {
+        let div = document.createElement("div");
+        let br = document.createElement("br");
+
+        div.innerText = m.title;
+        div.className = "label label-default";
+        output.appendChild(div);
+        output.appendChild(br);
+    });
+}
+
+loadWithFetch("movies.json");
+
+click.flatMap(e => loadWithFetch("movies.json"))
+    .subscribe(
+    renderMovies,
+    e => console.log(`error: ${e}`),
     () => console.log(`complete`)
 );
 
